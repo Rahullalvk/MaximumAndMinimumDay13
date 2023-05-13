f7844a73880977ebf66435409dbf862b2a04366f
Rahulalvk <rahulal432@gmail.com>
2023-05-13 08:13:03 -0700

    Given 3 Integers find the maximum

---
+++ MaximumAndMinimum/Maximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumAndMinimum
+{
+    public class Maximum
+    {
+        public static int GetMax(int num1, int num2, int num3)
+        {
+            int max = num1;
+
+            if (num2.CompareTo(max) > 0)
+            {
+                max = num2;
+            }
+
+            if (num3.CompareTo(max) > 0)
+            {
+                max = num3;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ MaximumAndMinimum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MaximumAndMinimum 
+{
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter the first integer:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second integer:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third integer:");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int max = Maximum.GetMax(num1, num2, num3);
+            Console.WriteLine("The maximum value is: " + max);
+        }
+    }
+}
